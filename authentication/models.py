5360b4b01d07cb5414f81e72dd2c8f832f7d9417
ilchyshyn-bohdan <43094332+ilchyshyn-bohdan@users.noreply.github.com>
2020-05-31 23:07:49 +0300

    Revert "Minor change"
    This reverts commit 834d558642f42d9d8c862ac9d201ed3631288630.

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.apps import apps
 
-# Playlist = apps.get_model('MusicApi', 'Playlist')
 
 class User(AbstractUser):
-    pl = models.ForeignKey('MusicApi.Playlist', on_delete=models.DO_NOTHING, null=True, blank=True)
+    pass
