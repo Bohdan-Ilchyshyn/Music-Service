84e2705253d0a8acbf6d02db13f7b218a9d48ff9
ilchyshyn-bohdan <43094332+ilchyshyn-bohdan@users.noreply.github.com>
2020-06-01 12:03:54 +0300

    Revert "Minor change"

--- MusicApi/models.py
+++ MusicApi/models.py
@@ -38,6 +38,7 @@ class Artist(models.Model):
 class Band(models.Model):
     name = models.CharField(max_length=200)
     cover = models.ImageField(upload_to=partial(make_filepath, 'band_covers/'))
+    history = models.TextField(blank=True)
 
 
 class Album(models.Model):
